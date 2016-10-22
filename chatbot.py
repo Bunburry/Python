65bc4dea356c4e4f0c7010fd4c3ef10f1666dd98
Gincė <Ginc>
2016-10-22 22:22:50 +0100

    Basic conversation

--- chatbot.py
+++ chatbot.py
@@ -20,21 +20,45 @@ for elem in newname:
 answer = raw_input()
 answer2 = answer.split()
 for i in answer2:
-	if answer2 not in stopwords or answer2 in ["good","great","not bad","very well"]:
+	if i in stopwords or i in ["good","great","not bad","very well"]:
 		print("I'm pleased to hear that!")
-	elif answer2 in ["bad","not well"]:
+	elif i in stopwords or i in ["bad","not well","terribly","terrible"]:
 		print("Hang in there, " + elem + ". Things will work out.")
-#if answer.lower() in ["good","great","not bad","very well"]:
-#	print("I'm pleased to hear that!")
-#elif answer.lower() in ["bad","not well"]:
-#	print("Hang in there, " + elem + ". Things will work out.")
+	else:
+		print("Do you want to hear a joke?")
+		joke = raw_input()
+		if joke.lower() in ["yes","sure","of course","yeah"]:
+			print("How many programmers does it take to change a light bulb? \nNone. It's a hardware problem.")
+		elif joke.lower() in ["no","not really","nope"]:
+			print("Why so serious?")
 	
-print("Do you want to hear a joke?")
-joke = raw_input()
-if joke.lower() in ["yes","sure","of course","yeah"]:
-	print("How many programmers does it take to change a light bulb? \nNone. It's a hardware problem.")
-elif joke.lower() in ["no","not really","nope"]:
-	print("Why so serious?")
+while True:
+	phrase = raw_input()
+	phrase = phrase.lower()
+	if "" in phrase:
+		if "what" in phrase and "name" in phrase:
+			print ("I haven't got a name. Give me one.")
+			bot = raw_input()
+			bot2 = bot.split()
+			for x in bot2:
+				if x in stopwords:
+					pass
+				else:
+					print ("My name is " + x)
+		elif "old" in phrase:
+			print ("I'm two weeks old")
+		elif "how" in phrase and "you" in phrase:
+			print ("I'm very well, thank you")
+		elif "lol" in phrase:
+			print ("*laughs in robot*")
+		elif "nice" in phrase and "you" in phrase:
+			print ("The pleasure is mine")
+		elif "dunno" in phrase or "ok" in phrase:
+			print ("k k k")
+		else:
+			something = ["Code harder","I can't understand you","*sobs*"]
+			print (random.choice(something))
+			
 
 
 
